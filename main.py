9cee470cc159c382343f4a896bd9f9fa95174fcb
kubuszi <kubushii2@wp.pl>
2024-05-28 10:37:06 +0200

    Get internet download and upload data

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
 
 PROMISED_DOWN = 150
 PROMISED_UP = 10
@@ -7,13 +11,24 @@ TWITTER_PASSWORD = ""
 
 
 class InternetSpeedTwitterBot:
-    def __init__(self, driver_path):
+    def __init__(self):
         self.driver = webdriver.Chrome()
         self.up = 0
         self.down = 0
 
     def get_internet_speed(self):
-        pass
+        self.driver.get("https://www.speedtest.net")
+        sleep(2)
+        accept_button = self.driver.find_element(By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
+        accept_button.click()
+        start_button = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+        start_button.click()
+        sleep(50)
+
+        self.down = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+        self.up = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        print(self.up)
+        print(self.down)
 
     def tweet_at_provider(self):
         pass
